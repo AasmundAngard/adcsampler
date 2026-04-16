a98bcab7bc59740460b5a65fe365984eca5a5897
AasmundAngard <wefling@live.no>
2026-04-16 13:32:06 +0200

    feat: speedRecording software for standardised filenames and semi-automated measurements

---
+++ speedRecording.py
@@ -0,0 +1,57 @@
+import subprocess
+import numpy as np
+import os
+
+from utilities.raspi_import import raspi_import
+from utilities.findTheta import angleOfAttack,findTheta
+
+
+# Kode for måling av radar
+# Formål: Å gi standardiserte filnavn automatisk
+# Kjører 2 ADC-er tilkoblet PI og lagrer resultatet
+# Resultat: 2 tidsserier med reell (I-kanal) og 
+# imaginær (Q-kanal) radardata.
+
+# Spesifikt til forsøket:
+# Velg tre farter mot radar, og en fra radar.
+# Gjennomfør fire målinger med radar på hver fart
+# speed1 - speed3 er mot radar
+# speed4 er fra radar
+
+
+# Måletid i sekund
+measurementDuration = 2
+MEASUREMENT_FREQUENCY = 31250
+
+
+
+
+
+while True:
+    speedToTest = input("Hvilken fart (1-4) vil du teste: ")
+
+    if speedToTest in ["q","quit","close","end"]:
+        break
+
+    # Finn ubrukt filnavn for målinger
+    toFileName =None
+    tempName = "speed"+str(speedToTest)+".bin"
+
+    # Finn ledig fil-index for farten speedToTest
+    for i in range(10):
+        tempName = "speed"+str(speedToTest)+"_"+str(i)+".bin"
+        if not (os.path.isfile(tempName)):
+            toFileName = tempName
+            break
+        
+    del tempName
+
+    if toFileName == None:
+        break
+
+    # Kjør adc_sampler med 10 sekund sampling, og skriv til filnavn. Blokkerer til ferdig kjørt
+    subprocess.run(["./adc_sampler", str(MEASUREMENT_FREQUENCY*measurementDuration), toFileName], check=True)
+
+
+    theta = findTheta(toFileName)
+    print("theta: ",theta)
